eec4801a6c427092fb54ea60bfbc59dfffc19dbe
laurenjrichie <laurenjrichie@gmail.com>
2014-11-19 13:59:18 -0800

    added quotes to homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @quotes = [
+      '"Failure is not an option. Everyone has to succeed." - Arnold Schwartzenegger',
+      '"Your time is limited, so don\'t waste it living someone else\'s life." - Steve Jobs',
+      '"Better Ingredients, Better Pizza." - Papa John',
+    ]
   end
 end
