9939de3ea82e6e16ab175d3a08cdfde2f838c8c1
laurenjrichie <laurenjrichie@gmail.com>
2014-11-21 14:59:04 -0800

    added faq page and link on homepage

---
+++ app/controllers/faq_controller.rb
@@ -0,0 +1,3 @@
+class FaqController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'terms' => 'terms#service'
 
+  get 'faq' => 'faq#answers'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
