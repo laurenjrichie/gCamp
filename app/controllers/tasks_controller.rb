fa52a7da7d6ea43a9705916ee078dfd28986a96d
laurenjrichie <laurenjrichie@gmail.com>
2014-11-24 17:16:41 -0800

    added link to download tasks csv

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,15 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @tasks = Task.all
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        headers['Content-Disposition'] = "attachment; filename=\"task-list\""
+        headers['Content-Type'] ||= 'text/csv'
+      end
+    end
+
   end
 
   # GET /tasks/1
