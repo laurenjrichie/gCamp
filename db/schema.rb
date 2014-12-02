34038dec85371f5efdbdeb05d8a71863443bd7e4
laurenjrichie <laurenjrichie@gmail.com>
2014-12-01 22:13:44 -0800

    added completed checkbox to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124230342) do
+ActiveRecord::Schema.define(version: 20141202000134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141124230342) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "completed",   default: false
   end
 
 end
