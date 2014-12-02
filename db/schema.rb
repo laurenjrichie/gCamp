d8d362240a301e59df6c808359e276c60f2b656b
laurenjrichie <laurenjrichie@gmail.com>
2014-12-01 22:30:00 -0800

    added due date to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202000134) do
+ActiveRecord::Schema.define(version: 20141202061634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141202000134) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "completed",   default: false
+    t.date     "due_date"
   end
 
 end
