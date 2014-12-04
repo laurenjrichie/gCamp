09926092a0b63bd101d80ba1c5237446dae9346c
laurenjrichie <laurenjrichie@gmail.com>
2014-12-04 14:12:00 -0800

    users can update users, fixed doctor typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id]) # could refactor with before_action at top
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to users_path, notice: "User was succesfully updated"
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(
