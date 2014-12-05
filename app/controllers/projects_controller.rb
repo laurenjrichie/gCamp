8b66810d49499d5f8df99ad78f2f44cc4214f66e
laurenjrichie <laurenjrichie@gmail.com>
2014-12-05 13:41:09 -0800

    users can show, edit, delete projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,45 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path, notice: "Project was successfully created"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    if @project.save
+      redirect_to projects_path, notice: "Project was successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: "Project was successfully deleted"
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(
+      :name,
+    )
+  end
+
 end
