31814827c45fd53e950e8af1ef4bed97925a853c
laurenjrichie <laurenjrichie@gmail.com>
2014-12-04 19:02:26 -0800

    users can delete users from the edit screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "User was successfully deleted"
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
     params.require(:user).permit(
