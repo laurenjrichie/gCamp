d43e3bc25b929be426c04e97c3d54fb53e9c0fbb
laurenjrichie <laurenjrichie@gmail.com>
2014-12-11 12:54:49 -0800

    fixed task due date validation error

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,9 @@ class Task < ActiveRecord::Base
   validate :due_date_cannot_be_in_the_past, :on => :create
 
   def due_date_cannot_be_in_the_past
-    if due_date < Date.today
-      errors.add(:due_date, "can't be in the past")
+    if due_date.present? && due_date < Date.today
+        errors.add(:due_date, "can't be in the past")
     end
   end
+
 end
