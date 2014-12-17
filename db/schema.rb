6ccd1a75f93b66a29a9242ba639e2f1e094d4de9
laurenjrichie <laurenjrichie@gmail.com>
2014-12-16 16:34:57 -0800

    added bcrypt gem, sign up page pretty, user password column added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205205938) do
+ActiveRecord::Schema.define(version: 20141217002119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20141205205938) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
