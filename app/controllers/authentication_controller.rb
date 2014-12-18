25b90c0f97bbb76b0879076eb6cbecf286154055
laurenjrichie <laurenjrichie@gmail.com>
2014-12-17 19:45:24 -0800

    users can sign in

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,8 +1,8 @@
 class AuthenticationController < ApplicationController
 
   def create
-    @user = User.find_by_email(params[:email])
-    if @user && user.authenticate(params[:password])
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path
     else
