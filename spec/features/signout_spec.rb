42e50c9831470ab40602cb86d99b0ae8f58a8325
laurenjrichie <laurenjrichie@gmail.com>
2014-12-18 15:01:51 -0800

    added capybara coverage to sign out

---
+++ spec/features/signout_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'Signing out' do
+  scenario 'User can sign out after signing in' do
+    User.create!(
+      first_name: "New",
+      last_name: "Signout",
+      email: "new@signout.com",
+      password: "signoutpw",
+      password_confirmation: "signoutpw",
+    )
+
+    visit root_path
+    click_on "Users"
+    click_on "Sign In"
+    fill_in :email, with: "new@signout.com"
+    fill_in :password, with: "signoutpw"
+    click_on "signin-user-action"
+
+    expect(page).to have_content("New Signout")
+    expect(page).to have_content("Sign Out")
+
+    save_and_open_page
+    click_on "Sign Out"  # why signout_path not working here?
+    expect(page).to have_content("Sign In")
+    expect(page).to_not have_content("Sign Out")
+  end
+
+end
