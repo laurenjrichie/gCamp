b4d26a0e9113f18f7c980d1e2593802e8b774515
laurenjrichie <laurenjrichie@gmail.com>
2014-12-19 15:15:34 -0800

    fixed failing tasks tests

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 feature 'CRUDing tasks' do
   scenario 'Create a new task' do
+    Project.create!(name: "Testing creating task")
 
     visit root_path
-    click_on "Tasks"
+    click_on "Projects"
+    click_on "Testing creating task"
+    click_on "0 Tasks"
     click_on "New Task"
     fill_in :task_description, with: 'New Task'
     click_on "Create Task"
@@ -14,12 +17,13 @@ feature 'CRUDing tasks' do
   end
 
   scenario 'Edit a task' do
-    Task.create!(
-      description: "Task to Edit",
-    )
+    project = Project.create!(name: "Testing editing task")
+    project.tasks.create!(description: "Task to Edit")
 
     visit root_path
-    click_on "Tasks"
+    click_on "Projects"
+    click_on "Testing editing task"
+    click_on "1 Task"
     click_on "Edit"
     fill_in :task_description, with: 'Edited Task'
     click_on "Update Task"
@@ -29,47 +33,44 @@ feature 'CRUDing tasks' do
   end
 
   scenario 'List all tasks' do
-    Task.create!(
-      description: "Task",
-    )
-    Task.create!(
-      description: "Task2",
-    )
-    Task.create!(
-      description: "Task3",
-    )
+    project = Project.create!(name: "Testing listing tasks")
+    project.tasks.create!(description: "Task1")
+    project.tasks.create!(description: "Task2")
+    project.tasks.create!(description: "Task3")
 
     visit root_path
-    click_on "Tasks"
+    click_on "Projects"
+    click_on "Testing listing tasks"
+    click_on "3 Tasks"
 
-    expect(page).to have_content("Task")
+    expect(page).to have_content("Task1")
     expect(page).to have_content("Task2")
     expect(page).to have_content("Task3")
   end
 
   scenario 'Show a specific task' do
-    Task.create!(
-      description: "Show this task",
-    )
-    # Task.create!(
-      # description: "Dont show this task",
-    # )
+    project = Project.create!(name: "Testing showing a task")
+    project.tasks.create!(description: "Show this task")
+    # project.tasks.create!(description: "Dont show this task")
 
     visit root_path
-    click_on "Tasks"
+    click_on "Projects"
+    click_on "Testing showing a task"
+    click_on "1 Task"
     click_on "Show"
     expect(page).to have_content("Show this task")
     # expect(page).to_not have_content("Dont show this task")
   end
 
   scenario 'Delete task' do
-    Task.create!(
-      description: "Delete this task",
-    )
+    project = Project.create!(name: "Testing deleting a task")
+    project.tasks.create!(description: "Delete this task")
 
     visit root_path
-    click_on "Tasks"
-    click_on "Destroy"
+    click_on "Projects"
+    click_on "Testing deleting a task"
+    click_on "1 Task"
+    click_on "Delete"
 
     expect(page).to_not have_content("Delete this task")
     expect(page).to have_content("Task was successfully destroyed")
